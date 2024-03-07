9a2cbd5ee12a10f6452c6c985e282388d1888dc5
Prince Solomon <77865154+Princexz@users.noreply.github.com>
2024-03-07 17:07:14 +0100

    Add files via upload
    <h1 align="center">Quiz-Application</h1> 
     
    <p align="center">	
       <a href="https://www.linkedin.com/in/princexz">
          <img alt="Prince Solomon" src="https://img.shields.io/badge/-princexz-1AD043?style=flat&logo=Linkedin&logoColor=white" />
       </a>
      <a href="https://github.com/princexz/Quiz-Application/blob/main/License">
        <img alt="License: MIT" src="https://img.shields.io/github/license/princexz/Quiz-Application?color=#1AD043" />
      </a>
      <img alt="Stars" src= "https://img.shields.io/github/stars/princexz/Quiz-Application?color=#1AD043" />
      <img alt="Repo size" src="https://img.shields.io/github/repo-size/princexz/Quiz-Application?color=#1AD043" />
    <img alt= "Watchers" src="https://img.shields.io/github/watchers/princexz/Quiz-Application?color=#1AD043" />
    <img alt= "Last commit" src="https://img.shields.io/github/last-commit/princexz/Quiz-Application?color=#1AD043" />
    </p>
    
    
    > In this Quiz Application, users can respond to the quiz questions, and the quiz can be retaken after getting their responses. 
    
    > This Quiz Application is fully responsive, compatible with mobile devices.
    
    
    
    <div align="center">
    <img width="600" src="https://github.com/princexz/princexz/blob/main/Portfolio/img5.jpg" /> 
    <img width="600" src="https://github.com/princexz/princexz/blob/main/Portfolio/quiz.jpg" />
    <img width="600" src="https://github.com/princexz/princexz/blob/main/Portfolio/quiz1.jpg" />
    </div>
    
    
    <div align="center">
    <sub>This Quiz Application is created by
    <a href="https://github.com/princexz">Prince Solomon </a>
    </sub>
    </div>
    
    # 📝 Features 
    ✅ This is a quiz application website. <br>
    ✅ Each question is alloted 15 seconds, where user has to complete the quiz before time. 
    
    # 💻 Technologies used
    <img src="https://img.shields.io/badge/HTML5-FF3300?style=for-the-badge&logo=html5&logoColor=white">
    <img src="https://img.shields.io/badge/CSS3-0066FF?style=for-the-badge&logo=css3&logoColor=white">
    <img src="https://img.shields.io/badge/JavaScript-FFF600?style=for-the-badge&logo=javascript&logoColor=white">
    
    # 👩‍💻 IDE used
    <img src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white">
    
    # 🌐 Visit the website here
    <a href="https://princexz.github.io/Quiz-Application/">
    <img width="40" height="40" src="https://github.com/princexz/princexz/blob/main/Portfolio/github.png"></a>
    &nbsp;<a href="https://quizapplsite.netlify.app"><img width="40" height="40" src="https://github.com/princexz/princexz/blob/main/Portfolio/netlify.jpg"></a>
    &nbsp;<a href="https://quiz-application-pearl.vercel.app/"><img width="40" height="40" src="https://github.com/princexz/princexz/blob/main/Portfolio/vercel.png"></a>
    
    # 📕 License
    This project is under <a href="https://github.com/princexz/Quiz-Application/blob/main/LICENSE">MIT</a> license.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Quiz-Application
-n this Quiz Application, users can respond to the quiz questions, and the quiz can be retaken after getting their responses.   > This Quiz Application is fully responsive, compatible with mobile devices.
+<h1 align="center">Quiz-Application</h1> 
+ 
+<p align="center">	
+   <a href="https://www.linkedin.com/in/princexz">
+      <img alt="Prince Solomon" src="https://img.shields.io/badge/-princexz-1AD043?style=flat&logo=Linkedin&logoColor=white" />
+   </a>
+  <a href="https://github.com/princexz/Quiz-Application/blob/main/License">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/princexz/Quiz-Application?color=#1AD043" />
+  </a>
+  <img alt="Stars" src= "https://img.shields.io/github/stars/princexz/Quiz-Application?color=#1AD043" />
+  <img alt="Repo size" src="https://img.shields.io/github/repo-size/princexz/Quiz-Application?color=#1AD043" />
+<img alt= "Watchers" src="https://img.shields.io/github/watchers/princexz/Quiz-Application?color=#1AD043" />
+<img alt= "Last commit" src="https://img.shields.io/github/last-commit/princexz/Quiz-Application?color=#1AD043" />
+</p>
+
+
+> In this Quiz Application, users can respond to the quiz questions, and the quiz can be retaken after getting their responses. 
+
+> This Quiz Application is fully responsive, compatible with mobile devices.
+
+
+
+<div align="center">
+<img width="600" src="https://github.com/princexz/princexz/blob/main/Portfolio/img5.jpg" /> 
+<img width="600" src="https://github.com/princexz/princexz/blob/main/Portfolio/quiz.jpg" />
+<img width="600" src="https://github.com/princexz/princexz/blob/main/Portfolio/quiz1.jpg" />
+</div>
+
+
+<div align="center">
+<sub>This Quiz Application is created by
+<a href="https://github.com/princexz">Prince Solomon </a>
+</sub>
+</div>
+
+# 📝 Features 
+✅ This is a quiz application website. <br>
+✅ Each question is alloted 15 seconds, where user has to complete the quiz before time. 
+
+# 💻 Technologies used
+<img src="https://img.shields.io/badge/HTML5-FF3300?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/CSS3-0066FF?style=for-the-badge&logo=css3&logoColor=white">
+<img src="https://img.shields.io/badge/JavaScript-FFF600?style=for-the-badge&logo=javascript&logoColor=white">
+
+# 👩‍💻 IDE used
+<img src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white">
+
+# 🌐 Visit the website here
+<a href="https://princexz.github.io/Quiz-Application/">
+<img width="40" height="40" src="https://github.com/princexz/princexz/blob/main/Portfolio/github.png"></a>
+&nbsp;<a href="https://quizapplsite.netlify.app"><img width="40" height="40" src="https://github.com/princexz/princexz/blob/main/Portfolio/netlify.jpg"></a>
+&nbsp;<a href="https://quiz-application-pearl.vercel.app/"><img width="40" height="40" src="https://github.com/princexz/princexz/blob/main/Portfolio/vercel.png"></a>
+
+# 📕 License
+This project is under <a href="https://github.com/princexz/Quiz-Application/blob/main/LICENSE">MIT</a> license.
---
+++ js/questions.js
@@ -0,0 +1,585 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "B. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Text Preprocessor",
+      "B. Hyper Text Markup Language",
+      "C. Hyper Text Multiple Language",
+      "D. Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "D. Cascading Style Sheet",
+    options: [
+      "A. Common Style Sheet",
+      "B. Colorful Style Sheet",
+      "C. Computer Style Sheet",
+      "D. Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "A. Hypertext Preprocessor",
+    options: [
+      "A. Hypertext Preprocessor",
+      "B. Hypertext Programming",
+      "C. Hypertext Preprogramming",
+      "D. Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "D. Structured Query Language",
+    options: [
+      "A. Stylish Question Language",
+      "B. Stylesheet Query Language",
+      "C. Statement Question Language",
+      "D. Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "A. eXtensible Markup Language",
+    options: [
+      "A. eXtensible Markup Language",
+      "B. eXecutable Multiple Language",
+      "C. eXTra Multi-Program Language",
+      "D. eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does TLS stand for?",
+    answer: "B. Transport Layer Security",
+    options: [
+      "A. Transporting Layer Security",
+      "B. Transport Layer Security",
+      "C. Transportation Layer Security",
+      "D. Transport Lower Security"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does FTP stand for?",
+    answer: "C. File Transfer Protocol",
+    options: [
+      "A. File Transforming Protocol",
+      "B. File Transfer Protection",
+      "C. File Transfer Protocol",
+      "D. File Transfering Protocol"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does URL stand for?",
+    answer: "C. Uniform Resources Locator",
+    options: [
+      "A. Uniform Restoration Lacation",
+      "B. Uniforming Resources Layer",
+      "C. Uniform Resources Locator",
+      "D. Uniform Resources Layout"
+    ]
+  },
+  {
+    numb: 9,
+    question: "In 1969 the precursor of internet is born:",
+    answer: "B. ARPAnet",
+    options: [
+      "A. Google",
+      "B. ARPAnet",
+      "C. Internet",
+      "D. Network"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does DNS stand  for?",
+    answer: "D. Domain Name Saver",
+    options:[
+      "A. Domain Not Save",
+      "B. Donating Nothing Saver",
+      "C. Domain Name Save",
+      "D. Domain Name Saver"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What does ISP stand for?",
+    answer: "C. Internet Service Provider",
+    options: [
+      "A. Internet Service Protocol",
+      "B. Internal Service Provider",
+      "C. Internet Service Provider",
+      "D. Internet Service Protocol"
+    ]
+  },
+  {
+    numb: 12,
+    question: "The Web Technology is made up of?",
+    answer: "C. HTML, CSS, Javascript",
+    options:[
+      "A. HTML, Python, React,",
+      "B. Css, python, Javascript",
+      "C. HTML, CSS, Javascript",
+      "D. Database, Websaver, Domain"
+    ]
+  },
+  {
+    numb: 13,
+    question: "__ is the global system of interconnectioned computer networks.",
+    answer: "D. Internet",
+    options: [
+      "A. Network",
+      "B. Browser",
+      "C. Website",
+      "D. Internet"
+    ]
+  },
+  {
+    numb: 14,
+    question:" Which of the Hyperlink Uses a Word or Phrase?..",
+    answer: "A. Text Hyperlink",
+    options:[
+      "A. Text Hyperlink",
+      "B. Bookmark Hyperlink",
+      "B. E-mial Hyperlink",
+      "D. Image Hyperlink"
+    ]
+  },
+  {
+    numb: 15,
+    question: "An electronic tool that allows information to be input, processed, and output is called _______________",
+    answer: "D. Computer",
+    options:[
+      "A. Operating System",
+      "B. CPU",
+      "C. Motherboard",
+      "D. Computer"
+    ]
+  },
+  {
+    numb: 16,
+    question: "_____________ is a worldwide network of computers.",
+    answer: "C. Internet",
+    options:[
+      "A. Server",
+      "B. Router",
+      "C. Internet",
+      "D. RAM"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Name the brain of the computer that does the calculation, moving, and processing of information.",
+    answer: "D. CPU",
+    options:[
+      "A. Motherboard",
+      "B. RAM",
+      "C. Hard Disk",
+      "D. CPU"
+    ]
+  },
+  {
+    numb: 18,
+    question: "What is the main component of a computer?",
+    answer: "D. Processor",
+    options:[
+      "A. Battery",
+      "B. Screen",
+      "C. Keyboard",
+      "D. Processor"
+    ]
+  },
+  {
+    numb: 19,
+    question: "How many bits does a byte have?",
+    answer: "C. 8",
+    options:[
+      "A. 4",
+      "B. 5",
+      "C. 8",
+      "D. 10"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which type of memory has the fastest access time?",
+    answer: "C. Cache Memory",
+    options:[
+      "A. Random Access Memory (RAM)",
+      "B. Read-Only Memory (ROM)",
+      "C. Cache Memory",
+      "D. External Memory"
+    ]
+  },
+  {
+    numb: 21,
+    question: "What is the purpose of BIOS in a computer?",
+    answer: "B. Booting and Initializing the System",
+    options:[
+      "A. Protecting the Computer from Viruses",
+      "B. Booting and Initializing the System",
+      "C. Managing Disk Drives",
+      "D. Providing User Access to the System"
+    ]
+  },
+  {
+    numb: 22,
+    question: "What is the maximum number of disks that can be connected to a single motherboard?",
+    answer: "B. Two",
+    options:[
+      "A. One",
+      "B. Two",
+      "C. Four",
+      "D. Six"
+    ]
+  },
+  {
+    numb: 23,
+    question: "What is the name of the software that is responsible for managing the computer?",
+    answer: "D. Operating System (OS)",
+    options:[
+      "A. User Application Software",
+      "B. Kernel",
+      "C. Device Driver",
+      "D. Operating System (OS)"
+    ]
+  },
+  {
+    numb: 24,
+    question: "Which type of memory has a data retention rate of up to one year?",
+    answer: "D. RAM (Random Access Memory)",
+    options:[
+      "A. ROM (Read-Only Memory)",
+      "B. EPROM (Erasable Programmable Read-Only Memory)",
+      "C. EEPROM (Electrically Erasable Programmable Read-Only",
+      "D. RAM (Random Access Memory)"
+    ]
+  },
+  {
+    numb: 25,
+    question: "How many bits are used in the address bus of a typical computer system?",
+    answer: "B. 16 Bits",
+    options:[
+      "A. 8 Bits",
+      "B. 16 Bits",
+      "C. 24 Bits",
+      "D. 32 Bits"
+    ]
+  },
+  {
+    numb: 26,
+    question: "What is the term for the process by which a user can add new features or capabilities to an existing program?",
+    answer: "D. Maintenance",
+    options: [
+      "A. Development",
+      "B. Testing",
+      "C. Deployment",
+      "D. Maintenance"
+    ]
+  },
+  {
+    numb: 27,
+    question: "What is the purpose of the Interrupt Request (IRQ) line in a microprocessor?",
+    answer: "C. To signal an interrupt request from peripheral devices to the CPU.",
+    options:[
+      "A. To signal an interrupt request from the keyboard or other input/output devices to the CPU.",
+      "B. To signal an interrupt request from the CPU to the keyboard or other input/output devices",
+      "C. To signal an interrupt request from peripheral devices to the CPU.",
+      "D. To signal an interrupt request from the timer device to the CPU."
+    ]
+  },
+  {
+    numb: 28,
+    question: "Which type of memory has a write cycle time that is faster than read cycle time?",
+    answer: "D. DRAM (Dynamic Random Access Memory)",
+    options:[
+      "A .SRAM (Static Random Access Memory)",
+      "B. SDRAM (Single-Inferred DRAM)",
+      "C. DDR RAM (Double Data Rate RAM)",
+      "D. DRAM (Dynamic Random Access Memory)"
+    ]
+  },
+  {
+    numb: 29,
+    question: "The most Common Operating Sysytem for work station are......",
+    answer: "D. Unix and Windows NT",
+    options:[
+      "A. Dos and unix",
+      "B. Unix and Linux",
+      "C. Windows and Dos",
+      "D. Unix and Windows NT"
+    ]
+  },
+  {
+    numb: 30,
+    question: "Special purpose computer programs are sometimes called?...",
+    answer: "A. Firm Ware",
+    options:[
+      "A. Firm Ware",
+      "B. Mall Ware",
+      "C. Soft Ware",
+      "D. Hard Ware"
+    ]
+  },
+  {
+    numb: 31,
+    question: ".... Is a computer design to fit comfortably on top of a desk, typically with the monitor sitting on top of the computer",
+    answer: "C. Desktop Model",
+    options:[
+      "A. Tower Model",
+      "B. Top Computer",
+      "C. Desktop Model",
+      "D. Top Model"
+    ]
+  },
+  {
+    numb:32,
+    question: "......... Is a handheld device that combines computing, telephone/fax and networking features.",
+    answer: "A. Personal digital assistant (PDA)",
+    options:[
+      "A. Personal digital assistant (PDA)",
+      "B. Personal computer",
+      "C. Personal Information Computer",
+      "D. Personal assistant"
+    ]
+  },
+  {
+    numb:33,
+    question: "Information is intrinsically meaningless on its own and remains so unless interpreted by..",
+    answer: "C. Human beings",
+    options:[
+      "A. Computer",
+      "B. Softwares",
+      "C. Human beings",
+      "D. Technology"
+    ]
+  },
+  {
+    numb: 34,
+    question: "Mainframe computers are usually used in ......................... and as ................. in wide area networks",
+    answer: "B. Centralized database and as control nodes",
+    options:[
+      "A. Internet and Network",
+      "B. Centralized database and as control nodes",
+      "C. Programming and Coding",
+      "D. Database and Networking"
+    ]
+  },
+  {
+    numb: 35,
+    question: "............. is the electronic centerpiece of the computer",
+    answer:"A. Motherboard or Main board",
+    options:[
+      "A. Motherboard or Main board",
+      "B. Processor",
+      "C. Central Processing Unit (CPU)",
+      "D. Board"
+    ]
+  },
+  {
+    numb: 36,
+    question: ".......... Is any Process that a computer program does to enter data and summarize information",
+    answer: "B. Computer data processing",
+    options:[
+      "A. Data processing",
+      "B. Computer data processing",
+      "C. Processing data",
+      "D. Processing information"
+    ]
+  },
+  {
+    numb: 37,
+    question: "The programs that special purpose computers run are installed permanently into .............. so that they cannot be deleted, edited or updated.",
+    answer: "D. Read only memory",
+    options: [
+      "A. Read from computer",
+      "B. Random Access Memory",
+      "C. Hard Disk",
+      "D. Read only memory"
+    ]
+  },
+  {
+    numb: 38,
+    question: ".............. is a device that converts digital signals into analog.",
+    answer: "A. Moderm",
+    options:[
+      "A. Moderm",
+      "B. ROM",
+      "C. RAM",
+      "D. Data"
+    ]
+  },
+  {
+    numb: 39,
+    question: "The processing of data using manual method is likely to be influenced by............... which will in turn, distort the integrity or value of information.",
+    answer: "D. Human Error",
+    options: [
+      "A. Computer Error",
+      "B. Precessing Error",
+      "C. Data Error",
+      "D. Human Error"
+    ]
+  },
+  {
+    numb: 40,
+    question: "Depending on their flexibility in operation, computers are clasified as either ............. or .............",
+    answer: "A. Special purpose computers or general purpose computers",
+    options:[
+      "A. Special purpose computers or general purpose computers",
+      "B. Read only computers or Random Access Computers",
+      "C. Digital computers or Analog Computers",
+      "D. Portable computers or Personnal computers"
+    ]
+  },
+  {
+    numb: 41,
+    question: "At the time of plato..........were two erroneous applications of justice which were against the ethical value of Greek",
+    answer:"C. Thyrasymachus (means mighty or right)/ reciprocation of either good or evil to one's friend or foe respectively.",
+    options:[
+      "A. Thyrasyma (means mighty or right)/ reciprocation of either good or evil to one's friend or  friend of foe respectively.",
+      "B. Tyrasymachus (means mighty or right)/ reciprocation of either good or evil to one's friend",
+      "C. Thyrasymachus (means mighty or right)/ reciprocation of either good or evil to one's friend or foe respectively.",
+      "D. Thyrasymachus (means mighty or right)/ reciprocation of either evil or evil to one's friend or foe respectively."
+    ]
+  },
+  {
+    numb: 42,
+    question: "The soul has three parts which are ..........., ............. and ...............",
+    answer: "A. Rational, Spirited and appetitive parts",
+    options:[
+      "A. Rational, Spirited and appetitive parts",
+      "B. Rational, Demon and appetitive parts",
+      "C. Rattional, Spirited and appetitive parts",
+      "D. Rational, Spirited and approaching parts"
+    ]
+  },
+  {
+    numb: 43,
+    question: "For Aristotle, the state owes every citizen the common good which is .........................",
+    answer: "D. Happiness",
+    options:[
+      "A. Hope",
+      "B. Love",
+      "C. Joy",
+      "D. Happiness"
+    ]
+  },
+  {
+    numb: 44,
+    question: "The system of lawas a whole in the legal parlance is called............",
+    answer: "B. The system of justice",
+    options: [
+      "A. The system of Peace",
+      "B. The system of justice",
+      "C. system or justice",
+      "D. The system of wickedness"
+    ]
+  },
+  {
+    numb: 45,
+    question: "What a man is entitled to as a matter of justice is called ............",
+    answer: "A. Right",
+    options: [
+      "A. Right",
+      "B. Righthood",
+      "C. Wrong",
+      "D. Right law"
+    ]
+  },
+  {
+    numb: 46,
+    question: "............is the most universal and powerfull of all social institution.",
+    answer: "A. The state",
+    options: [
+      "A. The state",
+      "B. The Government",
+      "C. The president",
+      "D. The Governor"
+    ]
+  },
+  {
+    numb: 47,
+    question: "The four constituent of the modern state are ...............",
+    answer: "A. Population, territory, governmenty and sovereignt",
+    options:[
+      "A. Population, territory, governmenty and sovereignt",
+      "B. power, territory, governmenty and sovereignt",
+      "C. Population, territory, governmenty and School",
+      "D. Population, political, governmenty and sovereignt"
+    ]
+  },
+  {
+    numb: 48,
+    question: "Sovereignty is derived from the latin word.................... meaning supreme",
+    answer: "B. Superanus",
+    options: [
+      "A. Superman",
+      "B. Superanus",
+      "C. Supreme",
+      "D. Super"
+    ]
+  },
+  {
+    numb: 49,
+    question: "Assembly line, robots, rocket telemetry processing, flight system, industrial automation and control system are eaxamples of  ............ precessing.",
+    answer: "D. Real time Processing",
+    options: [
+      "A. Precessing",
+      "B. Information Processing",
+      "C. Automatic Processing",
+      "D. Real time Processing"
+    ]
+  },
+  {
+    numb: 50,
+    question: "Data in RAM is lost when the computer is .............",
+    answer: "C. Powered down",
+    options: [
+      "A. Powered on",
+      "B. switched off",
+      "C. Powered down",
+      "D. Switched on"
+    ]
+  },
+  {
+    numb: 51,
+    question: "The processing of data using manual method is likely to be influenced by............... which will in turn, distort the integrity or value of information.",
+    answer: "A. Human Error",
+    options: [
+      "A. Human Error",
+      "B. Precessing Error",
+      "C. Data Error",
+      "D. Computer Error"
+    ]
+  },
+  {
+    numb: 52,
+    question: ".......... Is any Process that a computer program does to enter data and summarize information",
+    answer: "A. Data processing",
+    options:[
+      "A. Data processing",
+      "B. Data progressing",
+      "C. Processing data",
+      "D. Processing information"
+    ]
+  },
+  {
+    numb: 53,
+    question: "......... is the term for the process by which a Programmer or a Developer can add new features or capabilities to an existing program?",
+    answer: "B. Maintenance",
+    options: [
+      "A. Assembly",
+      "B. Maintenance",
+      "C. Compiling",
+      "D. Editing"
+    ]
+  }
+];
